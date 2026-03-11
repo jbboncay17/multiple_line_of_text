861d637c056fed72091f4021bd64a4b25adaff4d
jbboncay17 <jbboncay1@gmail.com>
2026-03-11 16:03:18 +0800

    final

--- difference_of_two_numbers.py
+++ difference_of_two_numbers.py
@@ -1,5 +1,9 @@
-num1=int(input("Enter the first number:"))
-num2=int(input("Enter the second number:"))
+bold="\033[1m"
+end="\033[0m"
+
+num1=float(input(bold+"Enter the first number:"+end))
+num2=float(input(bold+"Enter the second number:"+end))
 
 diff=num1-num2
-print("The difference of two numbers is:",diff)
+print("")
+print(bold+"The difference of two numbers is:"+end,diff)
